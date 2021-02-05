b2d20f0a5456f4fca000f496a7b5f30e9bae577c
Craig Day <cday@zendesk.com>
2021-02-05 12:02:42 -0800

    mostly working sidebar nav

---
+++ assets/js/accordian.js
@@ -0,0 +1,28 @@
+const toggleAccordian = (elementId) => {
+  const collapsedClassName = 'chrome:sidebar_item_collapsed'
+  const expandedClassName = 'chrome:sidebar_item_expanded'
+  const accordianCollapsedClassName = 'chrome:sidebar_item_accordian_collapsed'
+  const accordianExpandedClassName = 'chrome:sidebar_item_accordian_expanded'
+  const btnEl = document.getElementById(elementId)
+  const accordian = document.getElementById(`${elementId}:accordian`)
+
+  if (btnEl.classList.contains(expandedClassName)) {
+    btnEl.classList.remove(expandedClassName)
+    btnEl.classList.add(collapsedClassName)
+    accordian?.classList?.remove(accordianExpandedClassName)
+    accordian?.classList?.add(accordianCollapsedClassName)
+  } else {
+    btnEl.classList.remove(collapsedClassName)
+    btnEl.classList.add(expandedClassName)
+    accordian?.classList?.remove(accordianCollapsedClassName)
+    accordian?.classList?.add(accordianExpandedClassName)
+  }
+}
+
+const init = () => {}
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    init()
+  }
+}
--- docs/getting_started.md
+++ docs/getting_started.md
@@ -1,4 +1,5 @@
 ---
+layout: default
 id: getting-started
 title: Getting Started
 ---
